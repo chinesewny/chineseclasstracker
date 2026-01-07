18fa08b29bef5fed1b206c8873aecf7512411831
chinesewny <chinesewny@gmail.com>
2026-01-07 20:11:29 +0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -49,7 +49,7 @@ export function calculateScores(studentId, tasks, scores) {
 
 export function escapeHtml(text) {
     if (!text) return '';
-    return text.replace(/&/g, "&amp;")
+    return String(text).replace(/&/g, "&amp;")
                .replace(/</g, "&lt;")
                .replace(/>/g, "&gt;")
                .replace(/"/g, "&quot;")
